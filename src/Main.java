9f1593c97618fc6f420273e16a0b01f38173daab
0871063 <c0871063@mylambton.ca>
2022-11-15 20:39:10 -0500

    Completion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 0;
+        int count = 0;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Provide the number : ");
+        number = keyboard.nextInt();
+
+        int actualNumber = number;
+
+        while (number > 0) {
+            count++;
+            if (number % 3 == 0) {
+                number = number + 4;
+            } else if (number % 4 == 0) {
+                number = number / 2;
+            } else if (number % 4 != 0 || number % 3 != 0) {
+                number = number - 1 ;
+            }
+        }
+        System.out.printf(" %d -> %d \n ", actualNumber , count);
     }
 }
